bbf56d8e05e96873d1969d0dd43d996ab0a739b5
borja <borja@helm.cat>
2018-01-25 11:35:22 +0100

    Fix blocking network issue

--- app/src/main/java/cat/helm/basearchitecture/dependencyinjection/application/ApplicationModule.kt
+++ app/src/main/java/cat/helm/basearchitecture/dependencyinjection/application/ApplicationModule.kt
@@ -2,10 +2,12 @@ package cat.helm.basearchitecture.dependencyinjection.application
 
 import android.app.Application
 import android.content.Context
+import cat.helm.basearchitecture.Android
 import cat.helm.basearchitecture.data.dependencyinjection.qualifier.ApplicationContext
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
+import kotlin.coroutines.experimental.AbstractCoroutineContextElement
 
 /**
  * Created by Borja on 21/12/16.
@@ -19,5 +21,9 @@ class ApplicationModule {
     internal fun provideContext(application: Application): Context {
         return application
     }
+
+    @Provides
+    @Singleton
+    internal fun providesContinuation(): AbstractCoroutineContextElement = Android
 
 }
